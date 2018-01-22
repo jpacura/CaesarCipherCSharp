065ba665727249f9651d6ee02b2d0eb52f112ec5
Joseph Pacura <jpacura96@gmail.com>
2018-01-21 21:12:46 -0500

    Add project files.

---
+++ CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class CaesarCipher
+    {
+        public static String encrypt(String text, int key)
+        {
+            String ret = "";
+
+            // Convert string to uppercase
+            text = text.ToUpper();
+
+            // Loop through the string one character at a time
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                // Skip character if it is not a capital letter
+                // This allows us to leave punctuation alone
+                if (c > 'Z' || c < 'A')
+                {
+                    ret = ret + c;
+                    continue;
+                }
+
+                // Add the key to the existing letter to shift it
+                char n = (char)(c + key);
+
+                // Subtract 26 from the character if it wraps around the alphabet
+                while (n > 'Z')
+                {
+                    n = (char)(n - 26);
+                }
+
+                ret = ret + n;
+            }
+
+            return ret;
+        }
+
+        public static String decrypt(String text, int key)
+        {
+            String ret = "";
+            text = text.ToUpper();
+
+            // Loop through the string one character at a time
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                // Skip character if it is not a capital letter
+                // This allows us to leave punctuation alone
+                if (c > 'Z' || c < 'A')
+                {
+                    ret = ret + c;
+                    continue;
+                }
+
+                // Subtract the key from the existing letter to shift it
+                char n = (char)(c - key);
+
+                // Add 26 to the character if it wraps around the alphabet
+                while (n < 'A')
+                {
+                    n = (char)(n + 26);
+                }
+
+                ret = ret + n;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ CaesarCipher/CaesarInteractive.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class CaesarInteractive
+    {
+        // This is the interactive console portion of this code
+        // It uses the other class for the encrypt and decrypt function
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caesar Cipher Interactive");
+            Console.WriteLine("By Joseph Pacura");
+            Console.Write("\n(E)ncrypt or (D)ecrypt?");
+            String selection = Console.ReadLine();
+            selection = selection.ToLower();
+            bool operation = false;
+            if (selection.Equals("e"))
+            {
+                operation = true;
+            }
+            else if (!selection.Equals("d"))
+            {
+                Console.WriteLine("Invalid Selection. Quitting");
+                Environment.Exit(0);
+            }
+
+            int k = 0;
+            bool keyValid = false;
+            do
+            {
+                Console.WriteLine("Enter the Key:");
+                String keyLine = Console.ReadLine();
+                keyValid = int.TryParse(keyLine, out k);
+            }
+            while(!keyValid);
+
+            Console.WriteLine("Enter the Text:");
+            String s = Console.ReadLine();
+
+            if (operation)
+            {
+                Console.WriteLine(CaesarCipher.encrypt(s, k));
+            }
+            else
+            {
+                Console.WriteLine(CaesarCipher.decrypt(s, k));
+            }
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
